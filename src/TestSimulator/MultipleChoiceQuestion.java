6a406012b9b11bf37172a33e8e53d4713f57f849
michael <michaellittle92@gmail.com>
2021-07-07 19:55:51 +1000

    Task 4 complete.

--- src/TestSimulator/MultipleChoiceQuestion.java
+++ src/TestSimulator/MultipleChoiceQuestion.java
@@ -1,4 +1,25 @@
 package TestSimulator;
 
-public class MultipleChoiceQuestion {
+public class MultipleChoiceQuestion extends Question {
+    String answers;
+    char correctAnswer;
+    char chosenAnswer;
+
+    String answer1;
+    String answer2;
+    String answer3;
+    String answer4;
+
+    @Override
+    boolean isAnswerCorrect() {
+        boolean answer;
+        if (chosenAnswer == correctAnswer){
+            answer = true;
+    }else{
+          answer = false;
+        }
+        return answer;
+    }
+
+
 }
