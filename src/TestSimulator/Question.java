fb7595a42d019aa5c814f5cdfa73d56e52e68d9f
michael <michaellittle92@gmail.com>
2021-07-07 19:47:10 +1000

    Task 3 complete.

--- src/TestSimulator/Question.java
+++ src/TestSimulator/Question.java
@@ -1,4 +1,23 @@
 package TestSimulator;
 
-public class Question {
+abstract class Question {
+    String questionID;
+    int chapterNumber;
+    String questionText;
+
+    public String getQuestionID() {
+        return questionID;
+    }
+
+    public int getChapterNumber() {
+        return chapterNumber;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    abstract boolean isAnswerCorrect();
+
+
 }
